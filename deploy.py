8701d557df7c386d4acae9f0426ae0a09d7c14e2
ulhangry <80541020+umjiwan@users.noreply.github.com>
2022-01-23 00:20:43 +0900

    Update Deploy file

--- deploy.py
+++ deploy.py
@@ -46,8 +46,7 @@ with open("README.md", "a") as file:
             before_year = set_post_name[:4]
             file.write(f"\n## {before_year}<br>\n")
 
-        file.write(f'\
-                        <a href="https://github.com/umjiwan/TIL/blob/main/post/{set_post_name}">{title}</a>\
+        file.write(f'<a href="https://github.com/umjiwan/TIL/blob/main/post/{set_post_name}">{title}</a>\
                          `{date}`\
                         <br>\n\
                     ')
